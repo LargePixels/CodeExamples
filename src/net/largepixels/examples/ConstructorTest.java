705bf5c092e23da61f4d718abddb62cba058b7b7
John Minchuk <john.minchuk@here.com>
2015-06-04 12:34:14 -0500

    Example of overloading a constructor causing the default to disappear

---
+++ src/net/largepixels/examples/ConstructorTest.java
@@ -0,0 +1,32 @@
+package net.largepixels.examples;
+
+/**
+ * 
+ * @author sporkit
+ * 
+ * Overload the constructor causes its default constructor to disappear.
+ * Overload: add arguments
+ * Override: replace implementation from a parent
+ *
+ */
+public class ConstructorTest {
+	
+	public ConstructorTest(String greeting) {
+		System.out.println(greeting);
+	}
+
+	public static void main(String[] args) {
+		ConstructorTest a = new ConstructorTest();  //this wont work, default constructor disappears after overloading
+		ConstructorTest b = new ConstructorTest("holler");
+	}
+	
+	/*
+	 * Output...
+	 * 
+	Exception in thread "main" java.lang.Error: Unresolved compilation problem: 
+	The constructor ConstructorTest() is undefined
+
+	at net.largepixels.examples.ConstructorTest.main(ConstructorTest.java:20)
+	 */
+
+}
