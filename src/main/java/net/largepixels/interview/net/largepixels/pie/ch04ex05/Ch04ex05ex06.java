0417d82232b467d1adbe77531a9b7bcd73ba6d64
johnminchuk <minchuk.john@gmail.com>
2015-10-26 08:37:22 -0500

    Un-flatten a linked list example.

--- src/main/java/net/largepixels/interview/net/largepixels/pie/ch04ex05/Ch04ex05ex06.java
+++ src/main/java/net/largepixels/interview/net/largepixels/pie/ch04ex05/Ch04ex05ex06.java
@@ -1,14 +1,15 @@
 package net.largepixels.interview.net.largepixels.pie.ch04ex05;
 
-public class Ch04ex05 {
+public class Ch04ex05ex06 {
 
     public static void main(String[] args) {
-        Ch04ex05 ch04ex05 = new Ch04ex05();
-        ch04ex05.runMe();
+        Ch04ex05ex06 ch04Ex05Ex06 = new Ch04ex05ex06();
+        ch04Ex05Ex06.runMe();
     }
 
     private class Node {
         Node next;
+        Node prev;
         Node child;
 
         String value;
@@ -43,12 +44,37 @@ public class Ch04ex05 {
 
         d.child = h;
 
+        b.prev = a;
+        c.prev = b;
+        d.prev = d;
+        f.prev = e;
+        g.prev = f;
+
         //a b  c  d
         //  efg   h
 
         flattenList(a);
 
         System.out.println("linked list starting at A is now flat");
+
+        unflattenList(a);
+
+        System.out.println("linked list starting at A is now un-flattened");
+    }
+
+    private Node unflattenList(Node head) {
+        if ( head == null ) {
+            return head;
+        }
+
+        if (head.child != null) {
+            Node childLast = fastForwardToChild(head);
+            head.next = childLast;
+        }
+
+        unflattenList(head.next);
+
+        return head;
     }
 
     public Node flattenList(Node head) {
@@ -72,6 +98,22 @@ public class Ch04ex05 {
 
     private Node fastFoward(Node head) {
         while ( head.next != null ) {
+            head = head.next;
+        }
+        return head;
+    }
+
+    private Node fastForwardToChild(Node head) {
+        Node stopNode = head;
+
+        if (head.next != null && head.prev == stopNode) {
+            return head.next.next;
+        }
+
+        while ( head.next != null ) {
+            if (head.prev == stopNode) {
+                break;
+            }
             head = head.next;
         }
         return head;
