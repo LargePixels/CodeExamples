d721895f8190d7551d1968a40b6195872cdd24d6
johnM <largepixelsnet@gmail.com>
2016-02-08 18:13:27 -0600

    Integer division example

--- src/main/java/net/largepixels/examples/DivisionExamples.java
+++ src/main/java/net/largepixels/examples/DivisionExamples.java
@@ -5,11 +5,31 @@ package net.largepixels.examples;
  */
 public class DivisionExamples {
 
+    /**
+     * demonstrate that integer math always rounds own, use modulus for the remainder
+     */
     private void runMe() {
-        System.out.println("5 / 3 = " + (5 / 3));
-        System.out.println("5 / 3 = " + (3 / 5));
 
+        //1.6
+        System.out.println("5 / 3 = " + (5 / 3));
+
+        //0.6
+        System.out.println("3 / 5 = " + (3 / 5));
+
+        //1 time with 2 left over
+        System.out.println("5 % 3 = " + ( 5 % 3));
+
+        //0 times with 3 left over
+        System.out.println("3 % 5 = " + ( 3 % 5));
     }
+
+    /*
+    output:
+        5 / 3 = 1
+        3 / 5 = 0
+        5 % 3 = 2
+        5 % 3 = 3
+     */
 
     public static void main(String args[]) {
         DivisionExamples divisionExamples = new DivisionExamples();
