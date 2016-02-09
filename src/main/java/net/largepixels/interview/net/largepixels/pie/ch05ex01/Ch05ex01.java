6bfd6b2f0f3f288d7a6b7cb05b279040557f34bf
johnM <largepixelsnet@gmail.com>
2016-02-08 20:23:09 -0600

    Binary search tree, pre order traversal

--- src/main/java/net/largepixels/interview/net/largepixels/pie/ch05ex01/Ch05ex01.java
+++ src/main/java/net/largepixels/interview/net/largepixels/pie/ch05ex01/Ch05ex01.java
@@ -2,6 +2,11 @@ package net.largepixels.interview.net.largepixels.pie.ch05ex01;
 
 /**
  * Created by minchuk on 11/4/2015.
+ *
+ * Create a binary search tree and perform a preorder traversal,
+ * notice how we create the BST we return head.  Java is "pass-by-copy"
+ * and that means we can't modify the "copied in" pointer to the
+ * original object
  */
 public class Ch05ex01 {
 
@@ -29,7 +34,19 @@ public class Ch05ex01 {
         head = insert(head, new Node(175));
         head = insert(head, new Node(110));
 
-        System.out.println("stop");
+        preOrderTraversal(head);
+
+    }
+
+    private void preOrderTraversal(Node head) {
+        System.out.println(head.value);
+
+        if (head.left != null) {
+            preOrderTraversal(head.left);
+        }
+        if (head.right != null) {
+            preOrderTraversal(head.right);
+        }
     }
 
     public Node insert(Node head, Node insert) {
