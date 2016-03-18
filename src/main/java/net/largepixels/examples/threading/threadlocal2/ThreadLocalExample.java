f47742c2c78677d1fc3ff71a70dfa2bbbd295461
LargePixels <largepixelsnet@gmail.com>
2016-03-18 09:03:11 -0500

    A much simpler threadlocal example

---
+++ src/main/java/net/largepixels/examples/threading/threadlocal2/ThreadLocalExample.java
@@ -0,0 +1,44 @@
+package net.largepixels.examples.threading.threadlocal2;
+
+/**
+ * Created by johnminchuk on 3/18/16.
+ *
+ * @see http://tutorials.jenkov.com/java-concurrency/threadlocal.html
+ */
+public class ThreadLocalExample {
+
+
+    public static class MyRunnable implements Runnable {
+
+        private ThreadLocal<Integer> threadLocal =
+                new ThreadLocal<Integer>();
+
+        @Override
+        public void run() {
+            threadLocal.set( (int) (Math.random() * 100D) );
+
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+            }
+
+            System.out.println(threadLocal.get());
+        }
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        MyRunnable sharedRunnableInstance = new MyRunnable();
+
+        Thread thread1 = new Thread(sharedRunnableInstance);
+        Thread thread2 = new Thread(sharedRunnableInstance);
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join(); //wait for thread 1 to terminate
+        thread2.join(); //wait for thread 2 to terminate
+    }
+
+
+}
